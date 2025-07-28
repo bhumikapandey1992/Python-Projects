3127b97bb9eef82de53b0683318de55eed4832dc
Bhumika Pandey <bhumikapandey@Bhumikas-MacBook-Pro.local>
2025-07-28 16:03:04 +0200

    commit abstract class exm

---
+++ abstractclassdemo.py
@@ -0,0 +1,28 @@
+# Abstract class - Think of them as templates for your other classes. 
+#                  They define what a class should do, but not how it does it
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius = radius
+    def area(self):
+        return 3.14 * self.radius ** 2
+    
+class Rectangle(Shape):
+    def __init__(self,width,height):
+        self.width = width
+        self.height = height
+    def area(self):
+        return self.width * self.height
+
+c = Circle(2)
+print(c.area())
+r = Rectangle(4,4)
+print(r.area())
+
